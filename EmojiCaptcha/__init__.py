dab451d29d69da068d9c5654a106cdbfa30a46e6
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2022-02-01 17:30:23 +0530

    fix-pil

--- EmojiCaptcha/__init__.py
+++ EmojiCaptcha/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from EmojiCaptcha.__main__ import Captcha
-
 __version__ = '1.0'
 __author__ = 'Jigar Varma <Jigarverma2002@gmail.com>'
 __homepage__ = 'https://github.com/Jigarvarma2005/EmojiCaptcha'
+
+from EmojiCaptcha.__main__ import Captcha
