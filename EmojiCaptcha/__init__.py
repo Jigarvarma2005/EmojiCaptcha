9cae0ba36e4d670b25c63d0d5f4afe3144ba1b24
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2022-02-01 17:39:19 +0530

    Update __init__.py

--- EmojiCaptcha/__init__.py
+++ EmojiCaptcha/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__version__ = '1.0'
+__version__ = '1.1'
 __author__ = 'Jigar Varma <Jigarverma2002@gmail.com>'
 __homepage__ = 'https://github.com/Jigarvarma2005/EmojiCaptcha'
 
